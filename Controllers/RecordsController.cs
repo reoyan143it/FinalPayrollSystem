9318f0e49ce29d0755c289e1744f1d60386e6abe
Rollway <reoyan143itme@gmail.com>
2021-12-04 12:11:34 +0800

    add edit and delete buttons to leave

--- Controllers/RecordsController.cs
+++ Controllers/RecordsController.cs
@@ -889,8 +889,8 @@ namespace FinalPayrollSystem.Controllers
                 var draw = Request.Form["draw"].FirstOrDefault();
                 var start = Request.Form["start"].FirstOrDefault();
                 var length = Request.Form["length"].FirstOrDefault();
-                var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
-                var sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
+                var sortColumn = Request.Form["columns[" + Request.Form["order[1][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+                var sortColumnDirection = Request.Form["order[1][dir]"].FirstOrDefault();
                 var searchValue = Request.Form["search[value]"].FirstOrDefault();
                 int pageSize = length != null ? Convert.ToInt32(length) : 0;
                 int skip = start != null ? Convert.ToInt32(start) : 0;
--- wwwroot/js/display-leaves.js
+++ wwwroot/js/display-leaves.js
@@ -3,20 +3,38 @@
         processing: true,
         serverSide: true,
         autoWidth: true,
-        ajax:{
+        orderMulti: true,
+        ordering: true,
+        search: {
+            "smart": true,
+            "caseInsensitive": true
+        },
+        ajax: {
             method: "POST",
             url: "/Records/LeaveList",
             dataType: "json",
         },
         columns: [
-            { "data": "leavehistid", "name": "ID"},
+            { "data": "leavehistid", "name": "ID" },
             { "data": "employeeid", "name": "Employee ID" },
             { "data": "employeename", "name": "Employee Name" },
             { "data": "appliedleave", "name": "Applied Leave" },
             { "data": "rempaidleave", "name": "Remain Paid Leave" },
             { "data": "isdeductible", "name": "Is Deductible" },
             { "data": "leavedatefrom", "name": "Leave Date from" },
-            { "data": "leavedateto", "name": "Leave Date to" }
+            { "data": "leavedateto", "name": "Leave Date to" },
+            {
+                "render": function (data, type, row, meta) {
+                    
+                    return "<a type='button' class='btn btn-primary' href='/Records/EditLeave?leavehistID=" + row.leavehistid + "'>EDIT</a> | " +
+                        "<a type='button' class='btn btn-danger' href='/Records/DeleteLeave?leavehistID='" + row.leavehistid + ">DELETE</a>";
+
+                }, "name":"Action"
+            }
+        ],
+        columnDefs: [
+            { "visible": false, "targets": [0]},
+            { "width": "15%", "searchable": false, "orderable": false, "targets": [8] },
         ]
     });
 
